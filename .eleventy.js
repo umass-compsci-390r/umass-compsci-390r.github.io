b600581dbe142d74175d4850ab87c68a9fc4b0e5
Tensor497 <ikeita@umass.edu>
2023-03-02 13:30:25 -0500

    Make resources passthrough.

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,8 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/css");
     eleventyConfig.addPassthroughCopy("./src/js");
-    eleventyConfig.addPassthroughCopy("./src/CNAME")
+    eleventyConfig.addPassthroughCopy("./src/CNAME");
+    eleventyConfig.addPassthroughCopy("./src/resources");
     return {
       dir: {
         input: "src",
